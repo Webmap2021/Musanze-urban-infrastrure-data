2557932732dd619f176b7e74a66bc4f947454536
Webmap2021 <76879681+Webmap2021@users.noreply.github.com>
2021-01-03 12:59:08 -0800

    Add files via upload

---
+++ data/healthfacilities_6.js
@@ -0,0 +1,12 @@
+var json_healthfacilities_6 = {
+"type": "FeatureCollection",
+"name": "healthfacilities_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "objectid": 153.0, "new_provin": "NORD", "district_n": "MUSANZE", "new_sector": "CYUVE", "unit_type": 2.0, "pbfphase": 2.0, "y": -1.45175, "x": 29.65267, "type": "Health Centre", "name": "Gasiza", "category": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 29.652669995889266, -1.451750061615583 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "objectid": 154.0, "new_provin": "NORD", "district_n": "MUSANZE", "new_sector": "KARWASA", "unit_type": 2.0, "pbfphase": 2.0, "y": -1.489633, "x": 29.67615, "type": "Health Centre", "name": "Karwasa", "category": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 29.676149995079903, -1.489633061912697 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "objectid": 157.0, "new_provin": "NORD", "district_n": "MUSANZE", "new_sector": "MUHOZA", "unit_type": 3.0, "pbfphase": 2.0, "y": -1.506, "x": 29.63662, "type": "Dispensaire", "name": "Ruhengeri", "category": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 29.638401934137764, -1.50832836113377 ] } },
+{ "type": "Feature", "properties": { "gid": 4.0, "objectid": 158.0, "new_provin": "NORD", "district_n": "MUSANZE", "new_sector": "MUHOZA", "unit_type": 1.0, "pbfphase": 2.0, "y": -1.505867, "x": 29.63673, "type": "District Hospital", "name": "Ruhengeri HD", "category": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 29.637996239008299, -1.507943393565536 ] } },
+{ "type": "Feature", "properties": { "gid": 5.0, "objectid": 159.0, "new_provin": "NORD", "district_n": "MUSANZE", "new_sector": "MUKO", "unit_type": 3.0, "pbfphase": 2.0, "y": -1.55535, "x": 29.59448, "type": "Dispensaire", "name": "Kabere", "category": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 29.594479995638697, -1.555350061911681 ] } }
+]
+}
---
+++ data/market_infrastrure_5.js
@@ -0,0 +1,10 @@
+var json_market_infrastrure_5 = {
+"type": "FeatureCollection",
+"name": "market_infrastrure_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "objectid": 18.0, "tradeid": 286.0, "tradecat": "Without market", "province": "North", "district": "Musanze", "sector": "Kimonyi", "cellule": "Kivumu", "longitude": 9833483.0, "latitude": 456227.4, "altitude": 1891.4, "covered": null, "paved": null, "stands": null, "category": 2.0, "name": "Kimonyi" }, "geometry": { "type": "Point", "coordinates": [ 29.606619931784, -1.506179313442415 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "objectid": 58.0, "tradeid": 449.0, "tradecat": "With market", "province": "North", "district": "Musanze", "sector": "Muhoza", "cellule": "Kigombe", "longitude": 9833125.0, "latitude": 460324.3, "altitude": 1833.0, "covered": "Yes", "paved": "Yes", "stands": "Yes", "category": 3.0, "name": "Musanze" }, "geometry": { "type": "Point", "coordinates": [ 29.643436983540688, -1.509423462438262 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "objectid": 75.0, "tradeid": 0.0, "tradecat": "With market", "province": "North", "district": "Musanze", "sector": "Muhoza", "cellule": "Mpenge", "longitude": 9833430.1, "latitude": 460143.5, "altitude": 0.0, "covered": "Yes", "paved": "Yes", "stands": "Yes", "category": 3.0, "name": "Musanze" }, "geometry": { "type": "Point", "coordinates": [ 29.641812953017432, -1.506663882177569 ] } }
+]
+}
